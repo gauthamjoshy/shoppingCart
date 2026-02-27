0ee3727c4f33a034622359c9976ccbf03d24c595
Gautham Joshy <gauthamjoshy32@gmail.com>
2026-02-27 14:35:27 +0530

    sample products added

--- src/products.ts
+++ src/products.ts
@@ -1,3 +1,40 @@
-import {Product} from "./types"
+import { Product } from "./types";
 
-const products: Product
+const products: Product[] = [
+    {
+        name: "IPhone 12",
+        id: 1001,
+        price: 45000,
+        category: "electronics",
+    },
+    {
+        name: "Shirt",
+        id: 1002,
+        price: 700,
+        category: "clothing",
+    },
+    {
+        name: "Burger",
+        id: 1003,
+        price: 250,
+        category: "food",
+    },
+    {
+        name: "Chinos",
+        id: 1004,
+        price: 1000,
+        category: "clothing",
+    },
+    {
+        name: "Telivision",
+        id: 1005,
+        price: 25000,
+        category: "electronics",
+    },
+    {
+        name: "Pizza",
+        id: 1006,
+        price: 100,
+        category: "clothing",
+    }
+];
